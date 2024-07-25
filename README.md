8d45a90d26dd238378ae5e93f1759bfa988bec5d
Arzaid-aka-Rz <arzaid010103@gmail.com>
2024-07-25 09:50:22 +0530

    TMDB API data Fetch && built MainContainer

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 ## Netflix GPT
 
-# Features
+### Features
+
 - Header
-- Login Form
-- Sign up Form
-- Firsebase setup
-- Create SignUp User Account
-- Implemented Sign Out 
-- Update Profile
+- App Routing
+- Authentication (Login, Sign Up, Form Validation)
+- Firebase Setup
+- User Account Management (Sign Up, Sign In, Sign Out, Profile Update)
+- Redux Store with userSlice
+- TMDB API Integration (Movies, Trailers)
+- Custom Hooks for Movie Data
+- State Management with Movie Data
+- YouTube Video Embedding (Autoplay, Mute)
+- Bug Fixes (User Display Name, Profile Picture, Auth Redirects)
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../utils/userSlice";
-import { auth } from "../utils/firebase";
 
 const Body = () => {
-  const dispatch = useDispatch();
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -20,23 +15,7 @@ const Body = () => {
     },
   ]);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const { uid, email, displayName, photoURL } = user;
-        dispatch(
-          addUser({
-            uid: uid,
-            email: email,
-            displayName: displayName,
-            photoURL: photoURL,
-          })
-        );
-      } else {
-        dispatch(removeUser());
-      }
-    });
-  }, []);
+ 
 
   return (
     <div>
