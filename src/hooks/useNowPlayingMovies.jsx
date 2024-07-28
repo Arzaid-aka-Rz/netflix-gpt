68dc4e8fc907e88ab9585de9cc9e9d7eac96e306
Arzaid-aka-Rz <arzaid010103@gmail.com>
2024-07-29 03:16:54 +0530

    updation using useMemo Hook

--- src/hooks/useNowPlayingMovies.jsx
+++ src/hooks/useNowPlayingMovies.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
@@ -8,6 +8,10 @@ const useNowPlayingMovies=()=>{
     // Fetch Data from TMDB API and update store
     const dispatch = useDispatch();
 
+    const nowPlayingMovies = useSelector(
+      (store) => store.movies.nowPlayingMovies
+    );
+
     const getNowPlayingMovies= async()=>{
         const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS);
         const json = await data.json();
@@ -16,7 +20,7 @@ const useNowPlayingMovies=()=>{
       };
     
       useEffect(()=>{
-        getNowPlayingMovies();
+        !nowPlayingMovies && getNowPlayingMovies();
       },[])
     
 
