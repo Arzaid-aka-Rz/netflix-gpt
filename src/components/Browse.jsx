88efa4de12f0d14195ff4facbc1ac26cbdd2e656
Arzaid-aka-Rz <arzaid010103@gmail.com>
2024-07-30 02:32:35 +0530

    updation

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,6 +1,6 @@
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import Header from "./Header";
-import MainContainer from "./MainContainer ";
+import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer ";
 import usePopularMovies from "../hooks/usePopularMovies";
 import GptSearch from "./GptSearch";
